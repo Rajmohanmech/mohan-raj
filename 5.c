60acf2df0ecd3d511a22d1f9b800e29bfb7684bb
Rajmohanmech <35652533+Rajmohanmech@users.noreply.github.com>
2018-01-23 16:48:25 +0530

    Create 5.c

---
+++ 5.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+void main()
+{
+int a,b,c;
+printf("ENTER THE a&b&c");
+scanf("%d\n%d\n%d",&a,&b,&c);
+if(a>b&&a>c)
+{
+printf(" \n A IS GREATER ");
+}
+else
+{
+printf("  \n A IS NOT GREATER");
+}
+}
+
