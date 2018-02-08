b3a26eaa12d93afb5314996d36a0ae285b1d4639
Rajmohanmech <35652533+Rajmohanmech@users.noreply.github.com>
2018-02-08 16:50:43 +0530

    Create 27.c

---
+++ 27.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+void main()
+{
+int a;
+printf("enter the value");
+scanf("\n%d",&a);
+if(a=a%100&&a!=0)
+{
+    printf("\nYes");
+}
+        else
+        {
+            printf("\nno");
+        }
+}
