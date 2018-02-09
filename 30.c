536d4fdc54c465a20b258af321be159d3a123fd8
Rajmohanmech <35652533+Rajmohanmech@users.noreply.github.com>
2018-02-09 16:29:37 +0530

    Create 30.c

---
+++ 30.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+void main()
+{
+      int a[1],n,i=0;
+    printf("Enter the value");
+    scanf("\n%d",&n);
+    {
+        scanf("\n%d",&a[i]);
+        a[i]=n;
+printf("\n%d\t%d",a[i],i);
+}
+}
+
