56a0e5da9f6ee330ff87b8575335ac95a3d6443c
Rajmohanmech <35652533+Rajmohanmech@users.noreply.github.com>
2018-02-13 16:16:52 +0530

    Create 37.c

---
+++ 37.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+void main()
+{
+int a,b,c;
+printf(" enter the a value");
+scanf("\n%d",&a);
+printf("enter the b value");
+scanf("\n%d",&b);
+ c=a;
+ a=b;
+ b=c;
+ {
+ printf("\n a=%d",a);
+ printf("\n b=%d",b);
+ }
+} 
