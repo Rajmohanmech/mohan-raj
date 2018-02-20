eae1a4de17279fdc1a6a45adb9e47c56eb645caf
Rajmohanmech <35652533+Rajmohanmech@users.noreply.github.com>
2018-02-20 15:49:32 +0530

    Create 32.c

---
+++ 32.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+#include<string.h>
+void main()
+{
+char a[200];
+int b=0,i;
+printf("Enter the string");
+scanf("\n%[^\n]",a);
+for(i=0;a[i]!='\0';i++)
+{
+if(a[i]==' ')
+b++;
+}
+printf("\nnumber of word is %d",b+1);
+}
