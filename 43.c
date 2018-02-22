758ae8b5d1dd5937b611801c41fb39332277e819
Rajmohanmech <35652533+Rajmohanmech@users.noreply.github.com>
2018-02-22 16:54:48 +0530

    Create 43.c

---
+++ 43.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+int main()
+{
+    char ch[10];
+    char ch1[20];
+   printf("enter the string");
+   scanf("%s",ch);
+   printf("\n enter the string");
+   scanf("%s",ch1);
+   printf("%s%s",ch,ch1);
+   }
+   }
