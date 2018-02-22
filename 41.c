80aec6aac498869498db90d5857dc31bd5fe3359
Rajmohanmech <35652533+Rajmohanmech@users.noreply.github.com>
2018-02-22 16:21:33 +0530

    Create 41.c

---
+++ 41.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+#include<string.h>
+void main()
+{
+char s[30];
+int i,k;
+printf("Enter the string");
+scanf("%s",s);
+printf("\nEnter the k value");
+scanf("\n%d",&k);
+for(i=0;i<=k;i++)
+{
+printf("\n%s",s);
+}
+}
