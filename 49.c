3167038f3368698319e324065a6ef599a8f37e67
Rajmohanmech <35652533+Rajmohanmech@users.noreply.github.com>
2018-03-01 15:57:37 +0530

    Create 49.c

---
+++ 49.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+void main()
+{
+    int a,b;
+    int c;
+    printf("\nEnter the a value");
+    scanf("%d",&a);
+    printf("\nEnter the b value");
+    scanf("%d",&b);
+    c=a+b;
+    printf("\nSum=%d",c);
+}
