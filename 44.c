4401ef7b79b1e0c8e2696c80121f67bdbb806e00
Rajmohanmech <35652533+Rajmohanmech@users.noreply.github.com>
2018-03-01 15:48:08 +0530

    Create 44.c

---
+++ 44.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+void main()
+{
+    int a;
+    printf("Enetr the a value");
+    scanf("%d",&a);
+    if(a>=1&&a<=10)
+{
+    printf("\nYes");
+}
+else
+{
+    printf("\nNo");
+}
+}
