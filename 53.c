b7a9c65a6f012a86f1f59acd420def2b34a2c792
Rajmohanmech <35652533+Rajmohanmech@users.noreply.github.com>
2018-03-03 13:13:25 +0530

    Create 53.c

---
+++ 53.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+void main()
+{
+    int a,b,c,d;
+    printf("Enter the a value");
+    scanf("%d",&a);
+    printf("\nEnter the b value");
+    scanf("%d",&b);
+    printf("\nEnter the c value");
+    scanf("%d",&c);
+    d=a+b+c;
+    printf("\n%d",d);
+}
