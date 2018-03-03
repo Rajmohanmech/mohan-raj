e4b9115488628877162ca2ff0e962eeb6b32ec24
Rajmohanmech <35652533+Rajmohanmech@users.noreply.github.com>
2018-03-03 13:29:47 +0530

    Create 55.c

---
+++ 55.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+void main()
+{
+int N,a,b;
+printf("ENTER THE a VALUE");
+scanf("%d",&a);
+printf("\nEnter the b value");
+scanf("%d",&b);
+N=a*b;
+if(N%2==0)
+{
+printf(" \n Even");
+}
+else
+{
+printf(" \n Odd");
+}
+}
