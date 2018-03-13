f4587bded891afe7ee4bb8f60eb215df6405280a
Rajmohanmech <35652533+Rajmohanmech@users.noreply.github.com>
2018-03-13 15:51:02 +0530

    Create 61.c

---
+++ 61.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+#include<string.h>
+void main()
+{
+char a[10];
+scanf("%s",&a);
+int i,n;
+printf("\nEnter the string");
+scanf("%d",&n);
+for(i=0;i<n;i++)
+{
+printf("\n%s",a[i]);
+}
+}
