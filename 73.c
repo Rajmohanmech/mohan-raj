3099ecf81fc5b257370ce1b4db5ab67e3e02d056
Rajmohanmech <35652533+Rajmohanmech@users.noreply.github.com>
2018-03-15 15:43:28 +0530

    Create 73.c

---
+++ 73.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+void main()
+{
+    int n,l,r;
+    scanf("%d",&n);
+    scanf("\n%d",&l);
+    scanf("\n%d",&r);
+    if(l<n&&n<r)
+    {
+        printf("\nYes");
+    }
+    else
+    {
+        printf("\nNO");
+    }
+    }
