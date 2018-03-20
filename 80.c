f89359e431e43ebac8a36b86a8c9f83502925665
Rajmohanmech <35652533+Rajmohanmech@users.noreply.github.com>
2018-03-20 16:25:18 +0530

    Create 80.c

---
+++ 80.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+void main()
+{
+    char a[10];
+    int b,c;
+    scanf("%s",a);
+    for(i=0;a[i]!='\0';i++)
+{
+    
+		b=(int)(a[i]);
+		if(b%2!=0)
+		{
+		    			printf("\n%c",a[i]);	
+
+}
