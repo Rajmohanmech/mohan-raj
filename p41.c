89aeb9fcb974b4b2d700ee8bacc3f41a69c9b6f8
Rajmohanmech <35652533+Rajmohanmech@users.noreply.github.com>
2018-03-20 16:32:10 +0530

    Create p41.c

---
+++ p41.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+void main()
+{
+    int N,K;
+    scanf("%d",&N);
+    scanf("%d",&K);
+    if(N%K==0)
+    {
+        printf("\nYes");
+    }
+    else
+    {
+        printf("\nNo");
+    }
+    }
